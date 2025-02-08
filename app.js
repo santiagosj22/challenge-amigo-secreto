e982895a78f0136f10043391932d20547a2707b5
Santiago <santiagosj01@gmail.com>
2025-02-08 12:12:00 -0500

    Creación Funcion Sortear Amigo

--- app.js
+++ app.js
@@ -3,16 +3,34 @@
 let amigos = []
 function agregarAmigo(){
     let inputUsuario = document.getElementById('amigo').value;
-    amigos.push(inputUsuario)
-    console.log(amigos)
-    limpiar('amigo')
 
-    
-
-    listaAmigos
+    // validacion de campos vacios
+    if(inputUsuario.trim() == ''){
+        alert("El campo no puede estar vacio")
+    }else{
+        amigos.push(inputUsuario)
+        console.log(amigos)
+        limpiar('amigo')
+        document.getElementById('listaAmigos').innerHTML = amigos.map(f => `<li>${f}</li>`).join("");
+    }
 }
 
 function limpiar(id){
     let campoLimpiar = document.getElementById(id);
     campoLimpiar.value = "";
 }
+
+
+function sortearAmigo(){
+    cantidadAmigos = amigos.length
+    indiceAleatorio = Math.floor((Math.random()*cantidadAmigos))//generar un numero aleatorio en 0 y la cantidad de amigos -1
+    console.log(cantidadAmigos)
+    console.log(indiceAleatorio)
+    console.log(amigos[indiceAleatorio])
+    let amigoSecreto = amigos[indiceAleatorio]
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = `El amigo secreto sorteado es: ${amigoSecreto}`;
+}
+
+
+// función amigo secreto es sortear al amigo, quitar todo lo de UL y mostrar el resutado "El amigo secretos sorteado es: "
