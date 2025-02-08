c43b95fcb8d16e981f785532d92ae426b401a64f
Santiago <santiagosj01@gmail.com>
2025-02-08 10:05:41 -0500

    Función boton agregarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []
+function agregarAmigo(){
+    let inputUsuario = document.getElementById('amigo').value;
+    amigos.push(inputUsuario)
+    console.log(amigos)
+    limpiar('amigo')
+
+    
+
+    listaAmigos
+}
+
+function limpiar(id){
+    let campoLimpiar = document.getElementById(id);
+    campoLimpiar.value = "";
+}
